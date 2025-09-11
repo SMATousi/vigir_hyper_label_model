e76a94b909b028f76ea7756080ccbc203c35dcf3
SMATousi <mohamadali.tousi@gmail.com>
2025-09-11 11:19:07 -0500

    performance update

--- experiments/performance_exp_transformer.py
+++ experiments/performance_exp_transformer.py
@@ -16,7 +16,7 @@ from transformer_models import LELATransformerWrapper
 
 lela = LELAWrapper(checkpoint_path="lela_checkpoint.pt") #load pretrained LELA model
 
-lela_transformer = LELATransformerWrapper(checkpoint_path="./model_checkpoints/model_transformer_0.pt", 
+lela_transformer = LELATransformerWrapper(checkpoint_path="./model_checkpoints/model_transformer_random.pt", 
 max_seq_len=800000, inference_max_seq_len=5000000)
 
 
@@ -39,6 +39,7 @@ datasets = [
 # dicts to save performance scores and runing times 
 rsts = defaultdict(list)
 running_times = defaultdict(list)
+
 for i in range(len(datasets)):
     rsts['dataset'].append(datasets[i])
     print(datasets[i])
