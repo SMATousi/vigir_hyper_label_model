8e874ed30ff6363659e6d0aacd6987abf19d99c6
SMATousi <mohamadali.tousi@gmail.com>
2025-10-06 16:49:16 -0500

    message passing sparse gnn added

--- train_bagGNN_eval_wandb.py
+++ train_bagGNN_eval_wandb.py
@@ -2,7 +2,7 @@ import torch.optim as optim
 import torch
 from torch.utils.data import DataLoader
 from data import DatasetOnlineGen
-from bag_attention_gnn import StackedBagAttentionGNN, BagAttentionGNNModelWrapper
+from bag_attention_gnn import StackedBagAttentionGNN, BagAttentionGNNModelWrapper, BagAttentionGNNWrapper
 from loss import  BCEMask
 import numpy as np
 from data import SytheticValidation, load_dataset_wrench
